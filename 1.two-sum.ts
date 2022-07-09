a52908e0405d76967af0af67dce0dae33f24c155
kotarosz727 <kotarosz727@gmail.com>
2022-07-09 16:42:38 +0900

    two-sum

---
+++ 1.two-sum.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map()
+    
+    for (let i=0; i < nums.length; i++){
+        if (map.has(target - nums[i])) return [map.get(target - nums[i]), i]
+        map.set(nums[i], i)
+    } 
+    
+    return null
+};
+// @lc code=end
+
