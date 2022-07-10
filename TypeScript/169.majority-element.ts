7d398674cf23d5f0fee251c4d46b22d9b4feaa9d
kotarosz727 <kotarosz727@gmail.com>
2022-07-10 16:40:53 +0900

    Majority Element  with majority algorithm

--- TypeScript/169.majority-element.ts
+++ TypeScript/169.majority-element.ts
@@ -6,24 +6,15 @@
 
 // @lc code=start
 function majorityElement(nums: number[]): number {
-    const map = new Map<number, number>()
-    let currentCount: number = 1
-    let maxCountSoFar: number = 1
     let majority: number = nums[0]
+    let count: number = 1
 
-    for (const num of nums){
-        if(map.has(num)){
-            currentCount = map.get(num) + 1
-            map.set(num, currentCount)
-        } else {
-            map.set(num, 1)
-        }
-
-        if (currentCount > maxCountSoFar) majority = num
-
-        maxCountSoFar = Math.max(maxCountSoFar, currentCount)
+    for (const num of nums.slice(1)){
+        if (majority === num) count += 1 
+        else if (count === 0) majority = num 
+        else count -= 1
     }
-
+    
     return majority
 };
 // @lc code=end
