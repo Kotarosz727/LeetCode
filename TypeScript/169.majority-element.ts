10f8a5240e6c741bba4dac41f302a6fa3fac823f
kotarosz727 <kotarosz727@gmail.com>
2022-07-10 17:11:42 +0900

    Majority Element another solution

--- TypeScript/169.majority-element.ts
+++ TypeScript/169.majority-element.ts
@@ -6,16 +6,7 @@
 
 // @lc code=start
 function majorityElement(nums: number[]): number {
-    let majority: number = nums[0]
-    let count: number = 1
-
-    for (const num of nums.slice(1)){
-        if (majority === num) count += 1 
-        else if (count === 0) majority = num 
-        else count -= 1
-    }
-    
-    return majority
-};
+    return nums.sort()[Math.floor(nums.length / 2)];
+}
 // @lc code=end
 
